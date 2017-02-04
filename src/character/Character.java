d17dba3a4b8651d2b3a3bbcfce13489b0945aafa
Supreet University <supreetuniversity@Thirdshelfs-MacBook-Pro-3.local>
2017-02-04 18:01:07 -0500

    [REFACTOR] Simplifying character class to use it with xstream xml serializer

--- src/character/Character.java
+++ src/character/Character.java
@@ -8,12 +8,34 @@ import character.abilities.*;
  * @author supreetuniversity
  */
 public class Character {
-	private String CharacterClass;
-	private AbilitiesInterface[] Abilities;
-	private int Level;
-	private String[] Weapons;
-	private String[] Armour;
+	private String characterClass;
+	private String name;
+	private int level;
+	private int strength;
+	private int dexterity;
+	private int constitution;	
 	
+	public void setName(String name){
+		this.name = name;
+	}
 	
+	public void setCharacterClass(String characterClass){
+		this.characterClass = characterClass;
+	}
 	
+	public void setLevel(int level){
+		this.level = level;
+	}
+	
+	public void setStrength(int strength){
+		this.strength = strength;
+	}
+	
+	public void setDexterity(int dexterity){
+		this.dexterity = dexterity;
+	}
+
+	public void setConstitution(int constitution){
+		this.constitution = constitution;
+	}
 }
