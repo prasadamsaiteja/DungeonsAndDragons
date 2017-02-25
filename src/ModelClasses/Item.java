bcb0aab6c9cc11b5d8cc1b9eeed29478c72f5f37
apriyanka1608 <apriyanka1608@users.noreply.github.com>
2017-02-24 20:48:51 -0500

    Update Item.java

--- src/ModelClasses/Item.java
+++ src/ModelClasses/Item.java
@@ -16,6 +16,10 @@ public class Item {
       public String itemClass;     
       @XmlElement(name="itemLevel")   
       public int itemLevel;
+      private int armorClass;
+      private int strength;
+      private int damagebonus;
+      private int count;
       
       public Item(String itemName, ItemType itemType, String itemClass, int itemLevel){
           itemName = this.itemName;
@@ -23,5 +27,62 @@ public class Item {
           itemClass = this.itemClass;
           itemLevel = this.itemLevel;
       }
+   public Item()
+      {
+    	  
+      }
 
+      public String getItemName()
+      {
+    	  return itemName;
+      }
+      
+      public void values(){
+    	  
+    	  if(level<=4){
+    		  count = 1;
+    	  }
+    	  else if(level<=8){
+    		  count = 2;
+    	  }
+    	  else if (level<=12){
+    		  count = 3;
+    	  }
+    	  else if (level<=16){
+    		  count = 4;
+    	  }
+    	  else
+    		  count = 5;
+    	 
+      }
+ @SuppressWarnings("fallthrough") 
+ 
+      public void calculateValues()
+      {
+	      values();
+    	  switch(itemType)
+    	  {
+    	  case Belt :
+    		  strength = strength + count;
+    		  break;
+    	  case Weapon :
+    		damagebonus = damagebonus + count;
+    		  break;
+    	  case Armor :
+    		  armorClass = armorClass + count;
+    		  break;
+    	  case Ring :
+    		  armorClass = armorClass + count;
+    		  break;
+    	  case Shield :
+    		  armorClass = armorClass + count;
+    		  break;
+    	  case Boots :
+    		  armorClass = armorClass + count;
+    		  break;
+    	  case Helmet:
+    		  armorClass = armorClass + count;
+    	  }
+      }
+    	  
 }
